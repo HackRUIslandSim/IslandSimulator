a0e99f8bb9e72e8bb2b89d24900e068c9516cc58
unknown <acater@gmail.com>
2015-10-03 15:32:11 -0400

    Tilechecks

--- src/Generation.java
+++ src/Generation.java
@@ -5,4 +5,12 @@ public class Generation
 	{
 		
 	}
+	public static void smooth (Tile[][] tiles)
+	{
+		//Checks to see if the surronding blocks are water
+		for(int y = 0;y<tiles.length;y++)
+		{
+			for(int x  = 0;)
+		}
+	}
 }
