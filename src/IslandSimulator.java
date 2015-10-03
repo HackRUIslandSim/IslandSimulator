5ad9620814b79957ea670f03458c15074582224f
Ryguy9999 <ryancirincione@aol.com>
2015-10-03 13:02:55 -0400

    Change main class

---
+++ src/IslandSimulator.java
@@ -0,0 +1,11 @@
+import javax.swing.JPanel;
+
+public class IslandSimulator extends JPanel
+{
+	private static final long serialVersionUID = 3858735718453756678L;
+
+	public static void main(String[] args)
+	{
+		
+	}
+}
